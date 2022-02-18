d98e64466e40eb3f220d0bd8c8937e4e43866aa0
Olesia Yurchenko <olesya.yurchenko91@gmail.com>
2022-02-19 00:25:01 +0200

    css bugfix

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -6,7 +6,7 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     return (
         <div>
             {options.map(option => (
-                <button type="button" key={option} onClick={() => onLeaveFeedback(option)}>{option}</button>
+                <button className={s.button} type="button" key={option} onClick={() => onLeaveFeedback(option)}>{option}</button>
   ))}
         </div>
         
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -4,7 +4,7 @@ import s from './Section.module.css';
 
 const Section = ({ title, children }) => {
     return (
-        <section>
+        <section className={s.section}>
             <h2 className={s.title}>{title}</h2>
             {children}
         </section>
